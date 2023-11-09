b130ae7027e87b33f1589d1a41331d90305cbe1d
KanzyTawfik <114655121+KanzyTawfik@users.noreply.github.com>
2023-11-09 13:30:38 +0200

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,6 @@
+void LCD_Command( unsigned char cmnd );
+void LCD_Char( unsigned char data );
+void LCD_Init (void);
+void LCD_String (char *str);
+void LCD_String_xy (char row, char pos, char *str);
+void LCD_Clear();
